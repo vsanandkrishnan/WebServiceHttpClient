82dc68be78b691837baaa7d043166951c9c1affa
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-10-20 08:59:48 +0530

    Added/Deployed new Rest API for testing

--- WebServiceAutomation/GetAutoTests/GetTestClass.cs
+++ WebServiceAutomation/GetAutoTests/GetTestClass.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace WebServiceAutomation.GetAutoTests
     [TestClass]
     public class GetTestClass
     {
-        private string getUrl = @"https://reqres.in/api/users?page=2";
+        private string getUrl = @"http://localhost:8080/laptop-bag/webapi/api/all";
         [TestMethod]
         public void GetMethodwithUrl()
         {
@@ -24,13 +25,38 @@ namespace WebServiceAutomation.GetAutoTests
         public void GetMethodwithUri()
         {
             HttpClient client = new HttpClient();
-
             Uri getUri = new Uri(getUrl);
             Task<HttpResponseMessage> httpResponse = client.GetAsync(getUri);
-
             HttpResponseMessage httpResponseMessage=httpResponse.Result;
-
+            HttpStatusCode httpStatusCode = httpResponseMessage.StatusCode;
             Console.WriteLine(httpResponseMessage.ToString());
+
+            //Status code
+            Console.WriteLine("The status code is "+httpStatusCode);
+            Console.WriteLine("The status code is "+(int)httpStatusCode);
+
+            //Http Content
+            HttpContent responseContent = httpResponseMessage.Content;
+            Task<string> responseData=responseContent.ReadAsStringAsync();
+            string data=responseData.Result;
+            Console.WriteLine(data);
+
+
+
+            client.Dispose();
+        }
+
+        [TestMethod]
+        public void GetMethodwithInvalidUri()
+        {
+            HttpClient client = new HttpClient();
+            Uri getUri = new Uri(getUrl+"/random/7/k");
+            Task<HttpResponseMessage> httpResponse = client.GetAsync(getUri);
+            HttpResponseMessage httpResponseMessage = httpResponse.Result;
+            HttpStatusCode httpStatusCode = httpResponseMessage.StatusCode;
+            Console.WriteLine(httpResponseMessage.ToString());
+            Console.WriteLine("The status code is " + httpStatusCode);
+            Console.WriteLine("The status code is " + (int)httpStatusCode);
             client.Dispose();
         }
     }
