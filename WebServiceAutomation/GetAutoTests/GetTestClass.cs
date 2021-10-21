b35058a956b595220cbdf059aedbc9104dd388c1
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-10-21 09:40:06 +0530

    added headers for GET API

--- WebServiceAutomation/GetAutoTests/GetTestClass.cs
+++ WebServiceAutomation/GetAutoTests/GetTestClass.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -59,5 +60,57 @@ namespace WebServiceAutomation.GetAutoTests
             Console.WriteLine("The status code is " + (int)httpStatusCode);
             client.Dispose();
         }
+
+        [TestMethod]
+        public void TestGetAllEndPointInJsonFormat()
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestHeaders headers=client.DefaultRequestHeaders;
+            headers.Add("Accept", "application/json");
+
+            Uri getUri = new Uri(getUrl);
+            Task<HttpResponseMessage> httpResponseMessage = client.GetAsync(getUri);
+            HttpResponseMessage httpResponse = httpResponseMessage.Result;
+            HttpStatusCode httpStatusCode = httpResponse.StatusCode;
+
+            //Status code
+            Console.WriteLine("The status code is " + httpStatusCode);
+            Console.WriteLine("The status code is " + (int)httpStatusCode);
+
+            //Http Content
+            HttpContent responseContent = httpResponse.Content;
+            Task<string> responseData = responseContent.ReadAsStringAsync();
+            string data = responseData.Result;
+            Console.WriteLine(data);
+
+            client.Dispose();
+        }
+
+        [TestMethod]
+        public void TestGetAllEndPointInXmlFormat()
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestHeaders httpRequestHeaders = client.DefaultRequestHeaders;
+            httpRequestHeaders.Add("Accept", "application/xml");
+
+            Uri getUri = new Uri(getUrl);
+            Task<HttpResponseMessage> httpResponseMessage = client.GetAsync(getUri);
+            HttpResponseMessage httpResponse=httpResponseMessage.Result;
+            HttpStatusCode httpStatusCode = httpResponse.StatusCode;
+
+            //Status Code
+            Console.WriteLine("The status Code is => "+httpStatusCode );
+            Console.WriteLine("The Status code is => "+(int)httpStatusCode);
+
+            //Response Body
+            HttpContent httpContent = httpResponse.Content;
+            Task<string> responseData = httpContent.ReadAsStringAsync();
+            string data = responseData.Result;
+            Console.WriteLine(data);
+
+            client.Dispose();           
+        }
+
+
     }
 }
