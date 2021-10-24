0808aad07eaddbf43d84e60d97f7cce89e9019e2
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-10-24 08:00:15 +0530

    Started adding utilities

---
+++ WebServiceAutomation/Utilities/FileReaders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServiceAutomation.Utilities
+{
+    public class FileReaders
+    {
+        public dynamic JSONDeserializerFile(dynamic data)
+        {
+
+
+        }
+    }
+}
