8d205ff41f987c025a3d1889c0b2f04378ad9f36
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-11-01 09:29:00 +0530

    Added new Helper class for Http Methods

---
+++ WebServiceAutomation/Helper/Request/HttpClientHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebServiceAutomation.Model;
+
+namespace WebServiceAutomation.Helper.Request
+{
+    public class HttpClientHelper
+    {
+        private static HttpClient httpClient;
+        private static HttpRequestMessage httpRequestMessage;
+        private static RestResponse restResponse;
+
+
+        /// <summary>
+        /// Http Client Method
+        /// </summary>
+        /// <param name="httpHeaders"></param>
+        /// <returns></returns>
+        private static HttpClient AddHeadersAndCreateHttpClient(Dictionary<string,string> httpHeaders)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            if (httpHeaders != null)
+            {
+                foreach (string key in httpHeaders.Keys)
+                {
+                    httpClient.DefaultRequestHeaders.Add(key, httpHeaders[key]);
+                }
+            }
+            
+
+            return httpClient;
+        }
+
+        /// <summary>
+        /// Http Create Request Message
+        /// </summary>
+        /// <param name="requestUrl"></param>
+        /// <param name="httpMethod"></param>
+        /// <param name="httpContent"></param>
+        /// <returns></returns>
+        private static HttpRequestMessage CreateHttpRequestMessage(string requestUrl, HttpMethod httpMethod, HttpContent httpContent)
+        {
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(httpMethod, requestUrl);
+
+            if(!(httpMethod==HttpMethod.Post))
+                httpRequestMessage.Content = httpContent;
+
+            return httpRequestMessage;
+        }
+
+        /// <summary>
+        /// Resposne from APIs
+        /// </summary>
+        /// <param name="requestUrl"></param>
+        /// <param name="httpMethod"></param>
+        /// <param name="httpContent"></param>
+        /// <param name="httpHeaders"></param>
+        private static RestResponse SendRequest(string requestUrl, HttpMethod httpMethod, HttpContent httpContent,
+            Dictionary<string, string> httpHeaders)
+        {
+
+            httpClient = AddHeadersAndCreateHttpClient(httpHeaders);
+            httpRequestMessage = CreateHttpRequestMessage(requestUrl, httpMethod, httpContent);
+
+            try
+            {
+                Task<HttpResponseMessage> httpResponseMessage=httpClient.SendAsync(httpRequestMessage);
+                restResponse = new RestResponse((int)httpResponseMessage.Result.StatusCode, httpResponseMessage.Result.Content.ReadAsStringAsync().Result);
+            }catch(Exception e)
+            {
+                restResponse = new RestResponse(500, e.Message);
+            }
+            finally
+            {
+                //Null Check Operator
+                httpRequestMessage?.Dispose();
+                httpClient?.Dispose();
+
+            }
+
+            return restResponse;
+        }
+
+        public static RestResponse PerformGetRequest(string requestUrl, Dictionary<string, string> httpHeaders) => SendRequest(requestUrl, HttpMethod.Get, null, httpHeaders);
+    }
+}
--- WebServiceAutomation/PostEndPoint/TestPostEndPoint.cs
+++ WebServiceAutomation/PostEndPoint/TestPostEndPoint.cs
@@ -186,5 +186,7 @@ namespace WebServiceAutomation.PostEndPoint
                 }
             }
         }
+
+
     }
 }
