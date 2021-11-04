8cafb8bd495b408959e65c2f3347dd8bfb21a2e3
Anandkrishnan V S <vs.anandkrishnan@outlook.com>
2021-11-04 19:17:45 +0530

    Added new helper class to handle Response Data

---
+++ WebServiceAutomation/Helper/Responsedata/ResponsedDataHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WebServiceAutomation.Helper.Responsedata
+{
+    public class ResponsedDataHelper
+    {
+
+        /// <summary>
+        /// Deserializing JSON response data into class object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="responseData"></param>
+        /// <returns>Json Class <typeparamref name="T"/></returns>
+        public static T DeserializeJsonResponse<T>(string responseData) where T: class
+        {
+            return JsonConvert.DeserializeObject<T>(responseData);
+        }
+
+
+        /// <summary>
+        /// Deserializing XML response data into class object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="responseData"></param>
+        /// <returns>Xml Class <typeparamref name="T"/></returns>
+        public static T DesrializeXmlResponse<T>(string responseData) where T:class
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            TextReader textReader = new StringReader(responseData);
+            return (T)xmlSerializer.Deserialize(textReader);
+        }
+    }
+}
